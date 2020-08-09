476528086aba210cd00eba875154384a391ed2b4
Alessandro <alessandroadm@live.com>
2020-08-09 20:48:30 -0300

    fix(variaveis): adicionando nome do usuario do github membro a

--- src/js/varaiveis.js
+++ src/js/varaiveis.js
@@ -1,20 +1,22 @@
     //Suas variáveis abaixo
-
-let linkAvatarMemberA = "https://avatars1.githubusercontent.com/u/34426407?s=460&u=f254a9a50b99e2a8f63ce58958cb97089032e307&v=4";
-let linkAvatarMemberB = "https://avatars1.githubusercontent.com/u/56928555?s=460&u=b3796a627072a5edfc931b153db555f752491372&v=4";
-
-let linkGithubMemberA = "https://github.com/ALESSANDROLMENEZES";
-let linkGithubMemberB = "https://github.com/AndreSVyeyra";
     
-let nameMemberA = "Alessandro dev";
-let nameMemberB = "André Vieira";
-
-
-let comunnityName = "IDEV";
-
-let challengeTitle = "Variáveis";
-
-    let technologiesYouLearned = ["Git","GitHub","JavaScript"];
+    let linkAvatarMemberA = "https://avatars1.githubusercontent.com/u/34426407?s=460&u=f254a9a50b99e2a8f63ce58958cb97089032e307&v=4";
+    let linkAvatarMemberB = "https://avatars1.githubusercontent.com/u/56928555?s=460&u=b3796a627072a5edfc931b153db555f752491372&v=4";
+    
+    let linkGithubMemberA = "https://github.com/ALESSANDROLMENEZES";
+    let linkGithubMemberB = "https://github.com/AndreSVyeyra";
+    
+    let nameMemberA = "Alessandro dev";
+    let nameMemberB = "André Vieira";
+    
+    
+    let comunnityName = "IDEV";
+    
+    let challengeTitle = "Variáveis";
+    
+    let technologiesYouLearned = ["Git", "GitHub", "JavaScript"];
+    
+    let githubUserNameMemberA = "ALESSANDROLMENEZES";
     
     
     
