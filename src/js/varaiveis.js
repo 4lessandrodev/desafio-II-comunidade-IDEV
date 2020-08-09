7232fe4845a235a556f3eb58b6ff81ccf8eba929
Andre Vieira <andrevieira19992018@gmail.com>
2020-08-09 20:02:23 -0300

    fix(variaveis.js): Criei uma variavel let e coloquei o link da imagem do meu perfil do gitHub com bigode fini

--- src/js/varaiveis.js
+++ src/js/varaiveis.js
@@ -1,7 +1,7 @@
     //Suas variáveis abaixo
 
 let linkAvatarMemberA = "https://avatars1.githubusercontent.com/u/34426407?s=460&u=f254a9a50b99e2a8f63ce58958cb97089032e307&v=4";
-
+let linkAvatarMemberB = "https://avatars1.githubusercontent.com/u/56928555?s=460&u=b3796a627072a5edfc931b153db555f752491372&v=4";
     
     
     
