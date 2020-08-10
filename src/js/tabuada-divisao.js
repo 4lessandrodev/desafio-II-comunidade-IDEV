332dba42cc635f8e795412e398a25c85e92e8c7c
Andre Vieira <andrevieira19992018@gmail.com>
2020-08-09 22:04:34 -0300

    feat(tabuada): coloquei o restante da tabuada

---
+++ src/js/tabuada-divisao.js
@@ -0,0 +1,21 @@
+let totalGeral = 0;
+let fatorDivisorio = 7;
+let indice = 1;
+
+totalGeral += fatorDivisorio + indice;
+console.log(`${fatorDivisorio} / ${indice} : ${fatorDivisorio / indice}`);
+indice++;
+
+totalGeral += fatorDivisorio + indice;
+console.log(`${fatorDivisorio} / ${indice} : ${fatorDivisorio / indice}`);
+indice++;
+
+totalGeral += fatorDivisorio + indice;
+console.log(`${fatorDivisorio} / ${indice} : ${(fatorDivisorio / indice).toFixed(1)}`);
+indice++;
+
+totalGeral += fatorDivisorio + indice;
+console.log(`${fatorDivisorio} / ${indice} : ${fatorDivisorio / indice}`);
+indice++;
+
+console.log(" Total = " + totalGeral);
